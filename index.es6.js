9e88fc7b38417a410d5ef5625ee560081637c27a
abudaan <daniel@abumarkub.net>
2017-07-28 15:06:35 +0200

    update

--- index.es6.js
+++ index.es6.js
@@ -1,34 +1,28 @@
+
+const L = window.L; // coding like it is 1999
+
 window.addEventListener('DOMContentLoaded', () => {
 
     fetch('schiedam.vg.json')
-    .then((response) => {
-        if (response.status >= 400) {
-            throw new Error("Bad response from server");
-        }
-        return response.json();
-    })
-    .then((spec) => {
-        const view = new vega.View(vega.parse(spec))
-        .renderer('svg')
-        .initialize('#schiedam')
-        // .hover()
-        .run();
-    })
-    .catch(e => console.error(e))
+        .then((response) => {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        })
+        .then((spec) => {
+            const map = L.map('app');
+            let tiles = L.tileLayer(
+                'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+                    attribution: '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+                    maxZoom: 18
+                }
+            )
+            const vegaLayer = L.vegaLayer(spec).addTo(map);
 
-    fetch('world.vg.json')
-    .then((response) => {
-        if (response.status >= 400) {
-            throw new Error("Bad response from server");
-        }
-        return response.json();
-    })
-    .then((spec) => {
-        const view = new vega.View(vega.parse(spec))
-        .renderer('svg')
-        .initialize('#world')
-        // .hover()
-        .run();
-    })
-    .catch(e => console.error(e))
+            vegaLayer.renderer('svg')
+            .logLevel(vega.Debug)
+            // .run();
+        })
+        .catch(e => console.error(e))
 });
--- schiedam/index.js
+++ schiedam/index.js
@@ -1,26 +1,36 @@
-'use strict';
 
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
 
-    fetch('spec3.json').then(function (response) {
-        if (response.status >= 400) {
-            throw new Error("Bad response from server");
-        }
-        return response.json();
-    }).then(function (spec) {
-        var view = new vega.View(vega.parse(spec)).renderer('svg').initialize('#app')
-        // .hover()
-        // .logLevel(vega.Debug)
-        .run()
-        // .addSignalListener('container_data', (name, data) => {
-        //     console.log(name, data);
-        // })
+    fetch('spec3.json')
+        .then((response) => {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        })
+        .then((spec) => {
 
-        setTimeout(() => {
-            console.log(view.data('buurten'));
-        }, 400)
-    }).catch(function (e) {
-        return console.error(e);
-    });
+            // const map = L.map('app').setView([51.9309303, 4.3491274], 13);
+            // L.tileLayer(
+            //     'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+            //         attribution: '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+            //         maxZoom: 18
+            //     }
+            // ).addTo(map);
+
+            // const vegaLayer = L.vega(spec, {
+            //     // Make sure the legend stays in place
+            //     delayRepaint: true,
+            // }).addTo(map);
+
+            var view = new vega.View(vega.parse(spec)).renderer('svg').initialize('#app')
+                // .hover()
+                .run();
+
+            setTimeout(() => {
+                console.log(view.data('buurten'));
+            }, 500);
+
+        })
+        .catch(e => console.error(e))
 });
-
