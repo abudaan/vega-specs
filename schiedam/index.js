c4330a329ac6d4f7afac2f81bf47093471fae820
abudaan <daniel@abumarkub.net>
2017-08-04 20:56:32 +0200

    tooltip

--- schiedam/index.js
+++ schiedam/index.js
@@ -1,6 +1,24 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    // function addVgExample(path, id, options, tooltip = true) {
+    //     d3.json(path, function (error, vgSpec) {
+    //         if (error) {
+    //             return console.error(error);
+    //         }
+    //         var runtime = vega.parse(vgSpec);
+    //         var view = new vega.View(runtime)
+    //             .initialize(document.querySelector(id))
+    //             .hover()
+    //             .run();
+    //         if (tooltip === true) {
+    //             vegaTooltip.vega(view, options);
+    //         } else {
+    //             console.log('noop');
+    //         }
+    //     })
+
+
     fetch('spec3a.json')
         .then((response) => {
             if (response.status >= 400) {
@@ -19,11 +37,42 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
             ).addTo(map);
 
-            const vegaLayer = L.vega(spec, {
+            const l = L.vega(spec, {
                 // Make sure the legend stays in place
                 renderer: 'svg',
                 delayRepaint: true,
-            }).addTo(map);
+            })
+            map.on('layeradd', function (e) {
+                setTimeout(function () {
+                    // console.log('layer', e.layer);
+                    // console.log('spec', e.layer._spec);
+                    // console.log('map', e.layer._map);
+                    // console.log('view', e.layer._view);
+                    vegaTooltip.vega(e.layer._view, {
+                        showAllFields: false,
+                        fields: [
+                            {
+                                formatType: "number",
+                                field: "stortingen"
+                            },
+                            {
+                                formatType: "number",
+                                field: "vulling"
+                            },
+                            {
+                                formatType: "number",
+                                field: "meldingen"
+                            }
+                        ]
+                    });
+                }, 0)
+            })
+            arr = l.addTo(map);
+            // console.log(arr);
+            // setTimeout(function(){
+            //     var view = arr[1];
+            //     vegaTooltip.vega(view);
+            // }, 1000)
 
             // var i = 0;
             // var checker = setInterval(function(){
