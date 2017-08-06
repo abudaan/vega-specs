8d004b590f9d0c806150045d6362be98b054e441
abudaan <daniel@abumarkub.net>
2017-08-06 10:31:11 +0200

    add schools and containers and hovers

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import R from 'ramda';
 import TestSpec1 from '../specs/spec1';
 import TestSpec2 from '../specs/spec2';
 import TestSpec3 from '../specs/spec3';
+import TestSpec4 from '../specs/spec4';
 import createView from './util/create-vega-view';
 
 const toolTipOptions = {
@@ -23,7 +24,7 @@ const toolTipOptions = {
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-    createView(TestSpec3, 'app', 'canvas', false, (view) => {
+    createView(TestSpec4, 'app', 'canvas', true, (view) => {
         if (view && view._signals) {
             R.forEach((signal) => {
                 view.addSignalListener(signal, (name, data) => {
@@ -33,13 +34,17 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         if (view && view.data) {
             const stamp = Date.now();
-            const dataLoaded = setInterval(() => {
-                const reports = view.data('reports');
-                if (reports) {
-                    console.log(Date.now() - stamp, reports);
-                    clearInterval(dataLoaded);
-                }
-            }, 1);
+            // const dataLoaded = setInterval(() => {
+            //     try {
+            //         const reports = view.data('reports');
+            //         if (reports) {
+            //             console.log(Date.now() - stamp, reports);
+            //             clearInterval(dataLoaded);
+            //         }
+            //     } catch (e) {
+            //         console.error(e);
+            //     }
+            // }, 1);
         }
     });
 });
