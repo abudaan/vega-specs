8821373d8a76113ae8f204e915f2be8626bb59c6
abudaan <daniel@abumarkub.net>
2017-08-07 11:39:17 +0200

    fixed path issue

--- 4/build/app.js
+++ 4/build/app.js
@@ -10618,10 +10618,10 @@ exports.default = function () {
     var dataPath = window.location.protocol + '//' + window.location.host + '/data/';
     var imagePath = window.location.protocol + '//' + window.location.host + '/img/';
 
-    if (window.location.hostname === 'github') {
+    if (window.location.hostname.indexOf('github') !== -1) {
         dataPath = window.location.protocol + '//' + window.location.host + '/vega-specs/data/';
         imagePath = window.location.protocol + '//' + window.location.host + '/vega-specs/img/';
-    } else if (window.location.hostname === 'abumarkub') {
+    } else if (window.location.hostname.indexOf('abumarkub') !== -1) {
         dataPath = window.location.protocol + '//' + window.location.host + '/fffact/data/';
         imagePath = window.location.protocol + '//' + window.location.host + '/fffact/img/';
     }
