97bccde725372916a52c61ea92dae5e10a06051c
abudaan <daniel@abumarkub.net>
2017-08-07 11:38:25 +0200

    fixed path issue

--- src/util/config.js
+++ src/util/config.js
@@ -2,10 +2,10 @@ export default () => {
     let dataPath = `${window.location.protocol}//${window.location.host}/data/`;
     let imagePath = `${window.location.protocol}//${window.location.host}/img/`;
 
-    if (window.location.hostname === 'github') {
+    if (window.location.hostname.indexOf('github') !== -1) {
         dataPath = `${window.location.protocol}//${window.location.host}/vega-specs/data/`;
         imagePath = `${window.location.protocol}//${window.location.host}/vega-specs/img/`;
-    } else if (window.location.hostname === 'abumarkub') {
+    } else if (window.location.hostname.indexOf('abumarkub') !== -1) {
         dataPath = `${window.location.protocol}//${window.location.host}/fffact/data/`;
         imagePath = `${window.location.protocol}//${window.location.host}/fffact/img/`;
     }
